f99023637c20f8edd0f3ac8ea908d791163eef52
David Arroyo <daarroyo@ucm.es>
2019-02-15 16:19:09 +0100

    Barra búscar con botón y chips ingredientes

--- js/gastronomo.js
+++ js/gastronomo.js
@@ -1,1 +1,13 @@
 M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.chips');
+    var instances = M.Chips.init(elems, { onChipAdd: changeColor });
+
+});
+
+function changeColor() {
+    document.querySelectorAll('.chip').forEach(chip => {
+        chip.classList.add('red');
+    })
+}
