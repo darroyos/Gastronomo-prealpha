05b31e28fd0072c22550016513faa48533006bc9
David Arroyo <daarroyo@ucm.es>
2019-02-23 15:00:25 +0100

    Página de búsqueda

--- js/gastronomo.js
+++ js/gastronomo.js
@@ -12,3 +12,14 @@ function changeColor() {
         chip.classList.add('red');
     })
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function() {
+    $('select').formSelect();
+});
