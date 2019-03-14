7c81ab18077a168a830c4acb856e4a29d1186b44
David Arroyo <daarroyo@ucm.es>
2019-03-14 11:23:51 +0100

    Mejoras (solo importa recetas con pasos, img, ...)

--- RecetasImporter/import.py
+++ RecetasImporter/import.py
@@ -37,7 +37,7 @@ class Recipe(pw.Model):
     name = pw.CharField()
     url = pw.CharField()
     attribution = pw.CharField()
-    duration = pw.IntegerField()
+    duration = pw.CharField()
     calories = pw.FloatField()
     weight = pw.FloatField()
     rations = pw.FloatField()
@@ -122,31 +122,45 @@ Parser para Comida Kraft
 """
 
 
-def get_pasos_kraft(id_db, url, imagen):
+def parse_craft(id_db, url):
     # Proxy TOR necesario. Web con Geo Block (USA, Canada, etc.)
     session = requests.session()
     session.proxies = {}
     session.proxies['http'] = 'socks5h://localhost:9050'
     session.proxies['https'] = 'socks5h://localhost:9050'
+    info = dict()
 
     r = session.get(url)
 
-    soup = BeautifulSoup(r.text, 'html.parser')
-    pasos = soup.find_all("div", "krRecipeMakeItText")
-
-    pasos_json = dict()
-    paso_idx = 1
-
-    for paso in pasos:
-        paso = paso.text
-        pasos_json[str(paso_idx)] = paso
-        paso_idx += 1
-
-    r = session.get(imagen)
     if r.status_code == requests.codes.ok:
-        download_img(r.content, str(id_db) + '.' + file_extension(imagen))
+        
+        soup = BeautifulSoup(r.text, 'html.parser')
+        pasos = soup.find_all("div", 'krRecipeMakeItText')
+        raciones = soup.find('div', 'krRecipDescNutritionDescription')
+        imagen = soup.find('img', 'krBanImg')
+        imagen = 'https:' + imagen['src'] if 'https:' not in imagen['src'] else imagen['src']
+        duracion = soup.find('div', 'krRDPcookText')
 
-    return json.dumps(pasos_json, ensure_ascii=False).encode('utf8')
+        pasos_json = dict()
+        paso_idx = 1
+
+        for paso in pasos:
+            paso = paso.text
+            pasos_json[str(paso_idx)] = paso
+            paso_idx += 1
+
+        info['imagen'] = False
+        r = session.get(imagen)
+        if r.status_code == requests.codes.ok:
+            download_img(r.content, str(id_db) + '.' + file_extension(imagen))
+            info['imagen'] = True
+
+        info['pasos'] = json.dumps(pasos_json, ensure_ascii=False).encode('utf8')
+        info['raciones'] = int(raciones.text.strip().split(' ')[0])
+        info['duracion'] = duracion.text.strip()
+
+
+    return info
 
 
 """
@@ -154,42 +168,58 @@ Parser Que Rica Vida
 """
 
 
-def get_pasos_ricavida(id_db, url, imagen):
+def parse_ricavida(id_db, url):
     r = requests.get(url)
+    info = dict()
 
-    soup = BeautifulSoup(r.text, 'html.parser')
-    pasos = soup.find_all("div", "recipePartStepDescription")
-
-    pasos_json = dict()
-    paso_idx = 1
-
-    for paso in pasos:
-        paso = paso.text.strip()
-        pasos_json[str(paso_idx)] = paso
-        paso_idx += 1
-
-    r = requests.get(imagen)
     if r.status_code == requests.codes.ok:
-        download_img(r.content, str(id_db) + '.' + file_extension(imagen))
 
-    return json.dumps(pasos_json, ensure_ascii=False).encode('utf8')
+        soup = BeautifulSoup(r.text, 'html.parser')
+        pasos = soup.find_all("div", 'recipePartStepDescription')
+        raciones = soup.find(id='gmi_rp_primaryAttributes_porciones')
+        raciones = raciones.find_all('span')[1].text
+        imagen = soup.find(id='gmi_rp_recipeImage')
+        imagen = imagen.find('img')['src']
+        duracion = soup.find(id='gmi_rp_primaryAttributes_total')
+        duracion = duracion.find_all('span')
+        duracion = duracion[1].text + ' ' + duracion[2].text
+
+        info = dict()
+
+        pasos_json = dict()
+        paso_idx = 1
+
+        for paso in pasos:
+            paso = paso.text.strip()
+            pasos_json[str(paso_idx)] = paso
+            paso_idx += 1
+
+        info['imagen'] = False
+        r = requests.get(imagen)
+        if r.status_code == requests.codes.ok:
+            download_img(r.content, str(id_db) + '.' + file_extension(imagen))
+            info['imagen'] = True
+
+        info['pasos'] = json.dumps(pasos_json, ensure_ascii=False).encode('utf8')
+        info['raciones'] = int(raciones)
+        info['duracion'] = duracion
+
+    return info
 
 
 # Parsers de los pasos
 SCRAPPING = [
-    ('comidakraft.com', get_pasos_kraft),
-    ('quericavida.com', get_pasos_ricavida)
+    ('comidakraft.com', parse_craft),
+    ('quericavida.com', parse_ricavida)
 ]
 
 
-def procesar(recetas, dificultad):
+def procesar(idx, recetas, dificultad):
     duracion = recetas['params']['time']
     cocina = recetas['params']['q'][0]
     duracion = duracion[0] if len(
         duracion) == 1 else duracion[0] + '-' + duracion[1]
     recetas = recetas['hits']
-
-    idx = 1
 
     for r in recetas:
         receta = r['recipe']
@@ -200,6 +230,7 @@ def procesar(recetas, dificultad):
         calorias = receta['calories']
         peso = receta['totalWeight']
         raciones = receta['yield']
+        pasos = ""
 
         nueva = False
         try:
@@ -212,62 +243,67 @@ def procesar(recetas, dificultad):
             # Scraping de los pasos (solo si es una receta nueva, consume tiempo...)
             pasos_disponibles = [web[1] for web in SCRAPPING if web[0] in url]
             if len(pasos_disponibles) > 0:
-                pasos = pasos_disponibles[0](idx, url, imagen)
-            else:
-                pasos = ""
+                info = pasos_disponibles[0](idx, url)
 
-            receta_obj = Recipe.create(attribution=atribucion,
-                                       calories=calorias,
-                                       cuisine=cocina,
-                                       difficulty=dificultad,
-                                       duration=duracion,
-                                       name=nombre,
-                                       rations=raciones,
-                                       steps=pasos,
-                                       url=url,
-                                       weight=peso,
-                                       user_id=1)
+                disponible = 'raciones' in info and 'pasos' in info and 'duracion' in info and 'imagen' in info
 
-            tags = receta['dietLabels'] + receta['healthLabels']
+                if disponible and info['imagen']:
+                    raciones = info['raciones']
+                    pasos = info['pasos']
+                    duracion = info['duracion']
 
-            for t in tags:
+                    receta_obj = Recipe.create(attribution=atribucion,
+                                            calories=calorias,
+                                            cuisine=cocina,
+                                            difficulty=dificultad,
+                                            duration=duracion,
+                                            name=nombre,
+                                            rations=raciones,
+                                            steps=pasos,
+                                            url=url,
+                                            weight=peso,
+                                            user_id=1)
 
-                tag, creado = Tag.get_or_create(tag=t)
-                ingre_tag = RecipeTags.create(
-                    recipe_id=receta_obj.id, tags_id=tag.id)
+                    tags = receta['dietLabels'] + receta['healthLabels']
 
-            ingredientes = receta['ingredients']
+                    for t in tags:
 
-            for i in ingredientes:
-                nombre = i['text']
-                peso = float(i['weight'])
+                        tag, creado = Tag.get_or_create(tag=t)
+                        ingre_tag = RecipeTags.create(
+                            recipe_id=receta_obj.id, tags_id=tag.id)
 
-                ingrediente, creado = Ingredient.get_or_create(name=nombre)
-                receta_ingre = RecipeIngredient.create(weight=peso,
-                                                       ingredient_id=ingrediente.id,
-                                                       recipe_id=receta_obj.id)
+                    ingredientes = receta['ingredients']
 
-            nutrientes = receta['totalNutrients']
-            diario = receta['totalDaily']
+                    for i in ingredientes:
+                        nombre = i['text']
+                        peso = float(i['weight'])
 
-            for n in range(len(NUTRIENTES_ID)):
-                if NUTRIENTES_ID[n] in nutrientes and NUTRIENTES_ID[n] in diario:
-                    nutriente = nutrientes[NUTRIENTES_ID[n]]
-                    nutriente_diario = diario[NUTRIENTES_ID[n]]
-                    nombre = nutriente['label']
-                    cantidad = nutriente['quantity']
-                    unidad = nutriente['unit']
-                    porcentaje_diario = nutriente_diario['quantity']
-                    nutriente, creado = Nutrient.get_or_create(nutrient=nombre)
-                    receta_nutri = RecipeNutrient.create(cuantity=cantidad,
-                                                         daily_percentage=porcentaje_diario,
-                                                         unit=unidad,
-                                                         nutrient_id=nutriente.id,
-                                                         recipe_id=receta_obj.id)
-        idx += 1
+                        ingrediente, creado = Ingredient.get_or_create(name=nombre)
+                        receta_ingre = RecipeIngredient.create(weight=peso,
+                                                            ingredient_id=ingrediente.id,
+                                                            recipe_id=receta_obj.id)
+
+                    nutrientes = receta['totalNutrients']
+                    diario = receta['totalDaily']
+
+                    for n in range(len(NUTRIENTES_ID)):
+                        if NUTRIENTES_ID[n] in nutrientes and NUTRIENTES_ID[n] in diario:
+                            nutriente = nutrientes[NUTRIENTES_ID[n]]
+                            nutriente_diario = diario[NUTRIENTES_ID[n]]
+                            nombre = nutriente['label']
+                            cantidad = nutriente['quantity']
+                            unidad = nutriente['unit']
+                            porcentaje_diario = nutriente_diario['quantity']
+                            nutriente, creado = Nutrient.get_or_create(nutrient=nombre)
+                            receta_nutri = RecipeNutrient.create(cuantity=cantidad,
+                                                                daily_percentage=porcentaje_diario,
+                                                                unit=unidad,
+                                                                nutrient_id=nutriente.id,
+                                                                recipe_id=receta_obj.id)
+                    idx += 1
 
 
-def get_recipes(query, limite, tiempo, dificultad):
+def get_recipes(idx, query, limite, tiempo, dificultad):
     payload = {'app_id': APP_ID,
                'app_key': APP_KEY,
                'q': query,
@@ -277,7 +313,7 @@ def get_recipes(query, limite, tiempo, dificultad):
 
     r = requests.get(URL, params=payload)
 
-    procesar(r.json(), dificultad)
+    procesar(idx, r.json(), dificultad)
 
 
 def main(argv):
@@ -330,10 +366,19 @@ def main(argv):
     db.connect()
     db.create_tables([Recipe, Ingredient, Tag, RecipeIngredient,
                       RecipeTags, Nutrient, RecipeNutrient])
+
+    last_id = Recipe.select()
+    last_id = last_id.execute()
+    
+    """
     logger = logging.getLogger('peewee')
     logger.addHandler(logging.StreamHandler())
     logger.setLevel(logging.DEBUG)
-    get_recipes(query, limite, tiempo, dificultad)
+    """
+
+    idx = last_id[-1].id + 1 if last_id else 1
+
+    get_recipes(idx, query, limite, tiempo, dificultad)
     db.close()
 
 
